f9d9ca845673066bd1145a73a71ea324cac2f206
Jack Hickman <hickmanj@xtec2018mbl18.uni.ds.port.ac.uk>
2019-01-29 17:22:36 +0000

    minor thing - rhcp

--- phoneApp/PhoneApp.js
+++ phoneApp/PhoneApp.js
@@ -3,7 +3,7 @@ window.addEventListener("devicemotion", e => {
   console.log("Acceleration along the Y-axis " + e.acceleration.y);
   let power = Math.abs(e.acceleration.x) + Math.abs(e.acceleration.y);
 
-  if (power > 5) {
+  if (power > 12) {
     let element = document.querySelector(".mypp");
     element.innerHTML = `x = ${e.acceleration.x}</br>y = ${e.acceleration.y}`
     document.body.style.setProperty("background-color", "red");
