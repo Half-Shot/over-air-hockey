7b322794c571b0b01888ac6bbe5fb45288eda5b7
Jack Hickman <hickmanj@xtec2018mbl18.uni.ds.port.ac.uk>
2019-01-29 17:00:44 +0000

    One huge change

--- phoneApp/PhoneApp.js
+++ phoneApp/PhoneApp.js
@@ -4,5 +4,9 @@ accelerometer.addEventListener('reading', e => {
   console.log("Acceleration along the X-axis " + accelerometer.x);
   console.log("Acceleration along the Y-axis " + accelerometer.y);
   let power = Math.abs(accelerometer.x) + Math.abs(accelerometer.y);
+
+  if (power > 5) {
+    document.body.style.setProperty("background-color", "red");
+  }
 });
 accelerometer.start();
