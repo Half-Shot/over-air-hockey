017463ea738b2763d4b79fd3f42fede9b2371046
Jack Hickman <hickmanj@xtec2018mbl18.uni.ds.port.ac.uk>
2019-01-29 16:49:55 +0000

    Did an oopsie

--- PhoneApp.js
+++ PhoneApp.js
@@ -3,5 +3,6 @@ let accelerometer = new Accelerometer({frequency: 60});
 accelerometer.addEventListener('reading', e => {
   console.log("Acceleration along the X-axis " + accelerometer.x);
   console.log("Acceleration along the Y-axis " + accelerometer.y);
+  let power = Math.abs(accelerometer.x) + Math.abs(accelerometer.y);
 });
 accelerometer.start();
