afdd034425f735c8b65e9915f50b60901e7cd5f7
Will Hunt <will@half-shot.uk>
2019-01-29 17:04:28 +0000

    accelerate ur BEIG MEMs

--- phoneApp/PhoneApp.js
+++ phoneApp/PhoneApp.js
@@ -1,12 +1,9 @@
-let accelerometer = new Accelerometer({frequency: 60});
-
-accelerometer.addEventListener('reading', e => {
-  console.log("Acceleration along the X-axis " + accelerometer.x);
-  console.log("Acceleration along the Y-axis " + accelerometer.y);
-  let power = Math.abs(accelerometer.x) + Math.abs(accelerometer.y);
+window.addEventListener("devicemotion", e => {
+  console.log("Acceleration along the X-axis " + e.acceleration.x);
+  console.log("Acceleration along the Y-axis " + e.acceleration.y);
+  let power = Math.abs(e.acceleration.x) + Math.abs(e.acceleration.y);
 
   if (power > 5) {
     document.body.style.setProperty("background-color", "red");
   }
-});
-accelerometer.start();
+}, true);
